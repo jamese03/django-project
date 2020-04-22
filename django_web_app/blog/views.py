aca9ed50a8bb3cb3798df8c900ba0c55a669f5ed
James English <jenglish.ctr@kr.af.mil>
2020-04-22 17:22:08 -0400

    updating logic in html pages to conditionally render

--- django_web_app/blog/views.py
+++ django_web_app/blog/views.py
@@ -2,5 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': "James",
+        'title': 'First Blog',
+        'content': 'blog content',
+        'date_posted': "hardcoded date posted"
+    },
+    {
+        'author': "Not James",
+        'title': 'Second Blog',
+        'content': 'fake blog content v2',
+        'date_posted': "hardcoded date posted that is still wrong"
+    }
+]
+
 def home(request):
-    return HttpResponse("<h1> hello world </h1>")
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About page title'})
